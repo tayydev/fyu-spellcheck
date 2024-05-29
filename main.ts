7759990812211cbc8578c9a6aa00d1cb49ef1f1a
tayydev <51377975+tayydev@users.noreply.github.com>
2024-05-29 00:37:18 -0400

    Improved structure/organization

--- main.ts
+++ main.ts
@@ -1,53 +1,78 @@
-import {App, Editor, EditorPosition, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting} from 'obsidian';
+import {
+	App,
+	Editor,
+	EditorPosition,
+	MarkdownView,
+	Plugin,
+	PluginSettingTab,
+	Setting
+} from 'obsidian';
 import Typo from "typo-js";
 import {aff, dic} from "./data";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface FYUSpellcheckSettings {
+	// mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: FYUSpellcheckSettings = {
+	// mySetting: 'default'
 }
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+	settings: FYUSpellcheckSettings;
+	dictionary: Typo
+
+	correctWord(fromLeft: boolean, editor: Editor) {
+		const cursor: EditorPosition = editor.getCursor();
+		const line: string = editor.getLine(cursor.line);
+		const dict = this.dictionary
+
+		function didReplace(word: string): boolean {
+			// console.log("Processing Word: ", word)
+			if(dict.check(word)) return false;
+			else {
+				const corrected: string = dict.suggest(word).first() ?? word
+				const updatedLine: string = line.replace(word, corrected);
+				editor.replaceRange(updatedLine, { line: cursor.line, ch: 0 }, { line: cursor.line, ch: line.length });
+				return true;
+			}
+		}
+
+		const splitWords = line.split(/\s+/);
+
+		if(fromLeft) {
+			for (let i = 0; i < splitWords.length; i++) {
+				if(didReplace(splitWords[i])) break;
+			}
+		}
+		else {
+			for (let i = splitWords.length - 1; i >= 0; i--) {
+				if(didReplace(splitWords[i])) break;
+			}
+		}
+	}
 
 	async onload() {
 		await this.loadSettings();
 
 		//load library when plugin is loaded
-		const dictionary: Typo = new Typo(
+		this.dictionary = new Typo(
 			'en_US',
 			aff,
 			dic,
 			{}
 		)
 
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
 		this.addCommand({
 			id: 'spellcheck-leftmost',
 			name: 'Spellcheck leftmost word',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				const cursor: EditorPosition = editor.getCursor();
-				const line: string = editor.getLine(cursor.line);
-				const splitWords = line.split(/\s+/);
-				for(const word of splitWords) {
-					console.log("word", word)
-					if(!dictionary.check(word)) {
-						const corrected: string = dictionary.suggest(word).first() ?? word //TODO: Does this null check do anything
-						const updatedLine: string = line.replace(word, corrected);
-						editor.replaceRange(updatedLine, { line: cursor.line, ch: 0 }, { line: cursor.line, ch: line.length });
-						break;
-					}
-				}
+			editorCallback: (editor: Editor, _: MarkdownView) => {
+				this.correctWord(true, editor)
 			}
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new fyuSpellcheckSettingsTab(this.app, this));
 	}
 
 	onunload() {
@@ -63,23 +88,7 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
+class fyuSpellcheckSettingsTab extends PluginSettingTab {
 	plugin: MyPlugin;
 
 	constructor(app: App, plugin: MyPlugin) {
@@ -93,14 +102,7 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName('Settings are on their way...')
+			.setDesc('Eventually...')
 	}
 }
